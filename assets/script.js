ed3de82ddc423ffc9c1e96975342c9c0f81b9a0f
Elijah Walker <advancedathletix@gmail.com>
2022-01-19 11:34:04 -0500

    added submit button and next button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,39 @@
+// question index
+var currentQuestionIndex = 0;
 
+// question and answer array
+var questionArray = [
+    {
+        text: "What is 1 + 1?",
+        correctAnswer: 2,
+        choices: [
+            2, 5, 9, 6
+        ]
+    
+    }, {
+        text: "What is 10 + 10",
+        choices: [
+            25, 50, 90, 20
+        ],
+     
+        correctAnswer: 20
+    }, {
+        text: "what is 3 x 3",
+        choices: [
+            21, 12, 9, 33
+        ],
+
+        correctAnswer: 9
+    }, {
+        text: "what is 8 x 8",
+        choices: [
+            25, 64, 32, 20
+        ],
+        correctAnswer: 64
+
+    }
+]
+// end of array
 
 
 
